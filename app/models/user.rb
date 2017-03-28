6841c4b9ba7106f64bd40dd19e0ebda79c5507c2
ChocolateAceCream <ubuntu@ip-172-31-62-146.ec2.internal>
2017-03-28 18:32:21 +0000

    added validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, uniqueness: true
+    validates :email, uniqueness: { message: "Email has been taken" }, presence: { message: "Email is required"}
+    validates :name,  uniqueness: { message: "Name has been taken" }, presence: { message: "Name is required"}
+    validates :password, :presence => true,
+                         :confirmation => true,
+                         :length => {:within => 6..40},
+                         :on => :create
+
 end
