b9ee412200128fb84e566aadfdb6947be33083ef
ChocolateAceCream <ubuntu@ip-172-31-62-146.ec2.internal>
2017-03-29 13:03:30 +0000

    added email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, uniqueness: { message: "Email has been taken" }, presence: { message: "Email is required"}
+    validates :email, uniqueness: { message: "Email has been taken" },
+                      presence: { message: "Email is required"},
+                      format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :name,  uniqueness: { message: "Name has been taken" }, presence: { message: "Name is required"}
     validates :password, :presence => true,
                          :confirmation => true,
