c300bc18ae7d66c9552a217efe2ac90509453bed
ChocolateAceCream <ubuntu@ip-172-31-62-146.ec2.internal>
2017-03-31 20:04:46 +0000

    push before adding serializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: { message: "Email has been taken" },
-                      presence: { message: "Email is required"},
-                      format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+                      presence: { message: "Email is required"}
     validates :name,  uniqueness: { message: "Name has been taken" }, presence: { message: "Name is required"}
     validates :password, :presence => true,
                          :confirmation => true,
